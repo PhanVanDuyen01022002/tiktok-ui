789711b44e86572476584fd35d38b84ff185660f
PhanVanDuyen01022002 <phanvanduyen01022002@gmail.com>
2024-07-19 19:00:12 +0700

    Build UI header #6

---
+++ src/components/Image/index.js
@@ -0,0 +1,27 @@
+import { useState, forwardRef } from "react";
+import classNames from "classnames/bind";
+import images from "~/assets/images";
+import styles from "./Image.module.scss";
+
+const cx = classNames.bind(styles);
+function Image(
+  { src, alt, className, fallback: customFallback = images.noImage, ...props },
+  ref
+) {
+  const [fallback, setFallback] = useState("");
+  const handleError = () => {
+    setFallback(customFallback);
+  };
+  return (
+    <img
+      className={cx("wrapper", className)}
+      ref={ref}
+      src={fallback || src}
+      alt={alt}
+      {...props}
+      onError={handleError}
+    />
+  );
+}
+
+export default forwardRef(Image);
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,19 +1,10 @@
-import { Children, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faCircleQuestion,
   faCircleXmark,
-  faCloudUpload,
-  faCoins,
-  faEarthAsia,
   faEllipsisVertical,
-  faGear,
-  faKeyboard,
-  faMagnifyingGlass,
-  faSignOut,
   faSpinner,
-  faUser,
 } from "@fortawesome/free-solid-svg-icons";
 import Tippy from "@tippyjs/react";
 import HeadlessTippy from "@tippyjs/react/headless";
@@ -25,12 +16,26 @@ import styles from "./Header.module.scss";
 import images from "~/assets/images";
 import AccountItem from "~/components/AccountItem";
 import Menu from "~/components/Popper/Menu";
+import {
+  CoinIcon,
+  HelpIcon,
+  InboxIcon,
+  LanguageIcon,
+  LogoutIcon,
+  MessageIcon,
+  ProfileIcon,
+  SearchIcon,
+  SettingsIcon,
+  ShortcutsIcon,
+  UploadIcon,
+} from "~/components/Icon";
+import Image from "~/components/Image";
 
 const cx = classNames.bind(styles);
 
 const MENU_ITEM = [
   {
-    icon: <FontAwesomeIcon icon={faEarthAsia} />,
+    icon: <LanguageIcon />,
     title: "Tiếng Việt",
     children: {
       title: "Ngôn ngữ",
@@ -49,12 +54,12 @@ const MENU_ITEM = [
     },
   },
   {
-    icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+    icon: <HelpIcon />,
     title: "Phản hồi và trợ giúp",
     to: "/feedback",
   },
   {
-    icon: <FontAwesomeIcon icon={faKeyboard} />,
+    icon: <ShortcutsIcon />,
     title: "Phím tắt",
   },
 ];
@@ -82,23 +87,23 @@ function Header() {
 
   const userMenu = [
     {
-      icon: <FontAwesomeIcon icon={faUser} />,
+      icon: <ProfileIcon />,
       title: "Xem hồ sơ",
       to: "/@MeoConQT0",
     },
     {
-      icon: <FontAwesomeIcon icon={faCoins} />,
+      icon: <CoinIcon />,
       title: "Nhận xu",
       to: "/coin",
     },
     {
-      icon: <FontAwesomeIcon icon={faGear} />,
+      icon: <SettingsIcon />,
       title: "Cài đặt",
       to: "/settings",
     },
     ...MENU_ITEM,
     {
-      icon: <FontAwesomeIcon icon={faSignOut} />,
+      icon: <LogoutIcon />,
       title: "Đăng xuất",
       to: "/logout",
       separate: true,
@@ -131,7 +136,7 @@ function Header() {
             <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
 
             <button className={cx("search-btn")}>
-              <FontAwesomeIcon icon={faMagnifyingGlass} />
+              <SearchIcon />
             </button>
           </div>
         </HeadlessTippy>
@@ -145,7 +150,20 @@ function Header() {
                 placement="bottom"
               >
                 <button className={cx("action-btn")}>
-                  <FontAwesomeIcon icon={faCloudUpload} />
+                  <UploadIcon />
+                </button>
+              </Tippy>
+
+              <Tippy delay={[0, 200]} content="Tin nhắn" placement="bottom">
+                <button className={cx("action-btn")}>
+                  <MessageIcon />
+                </button>
+              </Tippy>
+
+              <Tippy delay={[0, 200]} content="Hộp thư" placement="bottom">
+                <button className={cx("action-btn")}>
+                  <InboxIcon />
+                  <div className={cx("inbox-number")}>16</div>
                 </button>
               </Tippy>
             </>
@@ -160,10 +178,11 @@ function Header() {
             onChange={handleMenuChange}
           >
             {currentUser ? (
-              <img
-                src="https://p16-sign-sg.tiktokcdn.com/aweme/720x720/tos-alisg-avt-0068/75bb784c32417b8b8574939ee27edb14.jpeg?lk3s=a5d48078&nonce=94286&refresh_token=ef2755cad7d0f032e8a769526cb12680&x-expires=1721545200&x-signature=9XTwt75p9WbenJ05osQBiDtbfUw%3D&shp=a5d48078&shcp=81f88b70"
+              <Image
+                src="ttps://p16-sign-sg.tiktokcdn.com/aweme/720x720/tos-alisg-avt-0068/75bb784c32417b8b8574939ee27edb14.jpeg?lk3s=a5d48078&nonce=94286&refresh_token=ef2755cad7d0f032e8a769526cb12680&x-expires=1721545200&x-signature=9XTwt75p9WbenJ05osQBiDtbfUw%3D&shp=a5d48078&shcp=81f88b70"
                 className={cx("user-avatar")}
                 alt="Phan Van Duyen"
+                fallback="https://fullstack.edu.vn/assets/f8-icon-lV2rGpF0.png"
               />
             ) : (
               <button className={cx("more-btn")}>
