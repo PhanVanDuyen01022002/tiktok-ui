4bbd3734422d6b3dc77902fb868254c3d0573d81
PhanVanDuyen01022002 <phanvanduyen01022002@gmail.com>
2024-07-22 17:55:34 +0700

    change file name

---
+++ src/layouts/components/Search/Search.js
@@ -0,0 +1,110 @@
+import { useEffect, useRef, useState } from "react";
+import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import HeadlessTippy from "@tippyjs/react/headless";
+import "tippy.js/dist/tippy.css";
+
+import * as searchServices from "~/services/searchService";
+import { Wrapper as PopperWrapper } from "~/components/Popper";
+import styles from "./Search.module.scss";
+import AccountItem from "~/components/AccountItem";
+import { SearchIcon } from "~/components/Icon";
+import { useDebounce } from "~/hooks";
+
+const cx = classNames.bind(styles);
+
+function Search() {
+  const [searchValue, setSearchValue] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [showResult, setShowResult] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  const debounced = useDebounce(searchValue, 500);
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (!searchValue) {
+      setSearchResult([]);
+      return;
+    }
+
+    const fetchApi = async () => {
+      setLoading(true);
+
+      const result = await searchServices.search(debounced);
+
+      setSearchResult(result);
+      setLoading(false);
+    };
+
+    fetchApi();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debounced]);
+
+  const handleChange = (e) => {
+    const searchValue = e.target.value;
+    if (!searchValue.startsWith(" ")) setSearchValue(searchValue);
+  };
+
+  const handleClear = () => {
+    setSearchValue("");
+    setSearchResult([]);
+    inputRef.current.focus();
+  };
+
+  const handleHideResult = () => {
+    setShowResult(false);
+  };
+
+  return (
+    //Using a wrapper <div> tag around the reference element solves
+    //this by creating a new parentNode context.
+    <div>
+      <HeadlessTippy
+        interactive
+        visible={showResult && searchResult.length > 0}
+        render={(attrs) => (
+          <div className={cx("search-result")} tabIndex="-1" {...attrs}>
+            <PopperWrapper>
+              <h4 className={cx("search-title")}>Tài khoản</h4>
+              {searchResult.map((result) => (
+                <AccountItem key={result.id} data={result} />
+              ))}
+            </PopperWrapper>
+          </div>
+        )}
+        onClickOutside={handleHideResult}
+      >
+        <div className={cx("search")}>
+          <input
+            ref={inputRef}
+            value={searchValue}
+            placeholder="Tìm kiếm"
+            spellCheck={false}
+            onChange={handleChange}
+            onFocus={() => setShowResult(true)}
+          />
+          {searchValue && !loading && (
+            <button className={cx("clear")} onClick={handleClear}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+          )}
+          {loading && (
+            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+          )}
+
+          <button
+            className={cx("search-btn")}
+            onMouseDown={(e) => e.preventDefault()}
+          >
+            <SearchIcon />
+          </button>
+        </div>
+      </HeadlessTippy>
+    </div>
+  );
+}
+
+export default Search;
